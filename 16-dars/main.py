767dc42518ee578905c31224bc7b55edd6425734
shohjahonsherzodov <uzbuzb314@gmail.com>
2025-08-15 09:13:45 +0500

    Add problem statements for lesson 16
    Added two programming tasks as comments in main.py: one for determining if a number is positive, negative, or zero, and another for calculating circle properties from a given radius.

---
+++ 16-dars/main.py
@@ -0,0 +1,26 @@
+# Masala:
+
+# musbat_manfiy funksiyasini yozing. Son kiritilsa, musbat/manfiy/0 ekanligini aniqlasin.
+
+# Funksiya ichida if-elif-else shartlari ishlatildi.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Masala
+
+# Foydalanuvchidan aylaning radiusini qabul qilib olib, uning radiusini, diametrini, perimetri va yuzini 
+# lug'at ko'rinishida qaytaruvchi funksiya yozing
